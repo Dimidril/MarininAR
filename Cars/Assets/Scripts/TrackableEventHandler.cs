09061385accee6cad0da13269330f5b221d63b47
Dmitry <Dimidril@users.noreply.github.com>
2021-06-28 21:12:08 +0300

    Программные доработки
    Добавлен выход и стартовая панель
     панель

---
+++ Cars/Assets/Scripts/TrackableEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackableEventHandler : DefaultTrackableEventHandler
+{
+    [SerializeField] private RectTransform _startPanel;
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        _startPanel.gameObject.SetActive(false);
+    }
+}

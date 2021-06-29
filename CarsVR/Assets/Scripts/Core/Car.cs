d35c236bec8dfcc02d7edbab80d84e7860b52e69
Dmitry <Dimidril@users.noreply.github.com>
2021-06-29 07:45:48 +0300

    Добавлены ресурсы VR

---
+++ CarsVR/Assets/Scripts/Core/Car.cs
@@ -0,0 +1,53 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private CarInaformation _info;
+    
+    private RectTransform _infoPanel;
+    private TMP_Text _infoText;
+
+    public CarInaformation Info => _info;
+
+    private void OnMouseDown()
+    {
+        _infoPanel.gameObject.SetActive(!_infoPanel.gameObject.activeInHierarchy);
+    }
+
+    public void OnCreate(RectTransform infoPanel, TMP_Text infoText)
+    {
+        _infoPanel = infoPanel;
+        _infoText = infoText;
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        EditText();
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void EditText()
+    {
+        _infoText.text = 
+            $"{Info.Mark} {Info.Model}\n" +
+            $"{Info.Description}\n" +
+            $"Максимальная скорость: {Info.MaxSpeed} км/ч\n" +
+            $"Разгон до 100 км/ч: {Info.TimeTo100kmInHour} cек.\n";
+    }
+}
+
+[System.Serializable]
+public struct CarInaformation
+{
+    public string Mark;
+    public string Model;
+    [TextArea(3, 10)] public string Description;
+    public float MaxSpeed;
+    public float TimeTo100kmInHour;
+}
